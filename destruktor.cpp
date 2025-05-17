bb700a67265aa57097b1a0be6f56363030a9d3eb
neiranie <nurkhairanianneira@gmail.com>
2025-05-17 14:03:46 +0700

    commit 6

---
+++ destruktor.cpp
@@ -0,0 +1,26 @@
+ #include <iostream>
+ using namespace std;
+
+ class angka{
+private:
+    int *arr;
+    int *panjang;
+public:
+    angka(int); //Constructor
+    ~angka(); //Destructor
+    void cetakData();
+    void isiData();
+ };
+ //Definisi member Function
+ angka::angka(int i){ //Constructor
+    panjang = i;
+    arr = new int[i];
+    isiData();
+ }
+ 
+ angka::~angka(){ //Destructor
+    cout<<endl;
+    cetakData();
+    delete[]arr;
+    cout<<"Alamat Array Sudah Dilepaskan"<<endl;
+ }
